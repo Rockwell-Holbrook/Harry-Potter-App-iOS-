3d973e79db07eac9a28f4097849a96ae42ae6437
Rockwell Holbrook <Rockwell.holbrook@gmail.com>
2018-09-12 12:52:33 -0600

    setup

---
+++ Harry_Potter/Harry_Potter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Harry_Potter
+//
+//  Created by Rockwell Holbrook on 9/12/18.
+//  Copyright © 2018 Rockwell Holbrook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
